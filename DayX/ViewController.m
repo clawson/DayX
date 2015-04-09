15b1e3d2ed1135f324448c3153e3a8f05636d4fd
Dan Clawson <clawson@uen.org>
2015-04-09 09:37:49 -0600

    Add steps 5-8; store data in NSUserDefaults.
    I didn't have a remote repo when during the first day of this project.  This branch has both lesson 5 and lesson 6 code.

--- DayX/ViewController.m
+++ DayX/ViewController.m
@@ -8,20 +8,69 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+static NSString * const entryKey = @"entry";
+static NSString * const titleKey = @"title";
+static NSString * const contentKey = @"body";
+
+@interface ViewController () <UITextFieldDelegate, UITextViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *titleTextField;
+@property (weak, nonatomic) IBOutlet UITextView *contentTextView;
+@property (weak, nonatomic) IBOutlet UIButton *clearButton;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void) viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // Remember, we must adopt each delegate
+    self.titleTextField.delegate = self;
+    self.contentTextView.delegate = self;
+    
+    // We need to load the journal entry from NSUserDefaults
+    NSDictionary *journalEntry = [[NSUserDefaults standardUserDefaults] objectForKey:entryKey];
+    [self updateViewWithDictionary:journalEntry];
 }
 
-- (void)didReceiveMemoryWarning {
+- (void) updateViewWithDictionary: (NSDictionary *) journalEntry {
+    self.titleTextField.text = journalEntry[titleKey];
+    self.contentTextView.text = journalEntry[contentKey];
+}
+
+- (IBAction) clearButtonPress: (id) sender {
+    self.titleTextField.text = @"";
+    self.contentTextView.text = @"";
+    
+}
+
+- (BOOL) textFieldShouldReturn: (UITextField *) titleField {
+    [titleField resignFirstResponder];
+    return YES;
+}
+
+-(void) textFieldDidEndEditing: (UITextField *) textField {
+    [self save:textField];
+}
+
+- (void) textViewDidChange: (UITextView *) textView {
+    [self save:textView];
+}
+
+- (void) save: (id)sender {
+    
+    // Convert our journalEntry to a dictionary to add it to NSUserDefaults
+    NSDictionary *journal = @{titleKey:self.titleTextField.text,  contentKey:self.contentTextView.text};
+    
+    [[NSUserDefaults standardUserDefaults] setObject:journal forKey:entryKey];
+    
+    // Write it immediately
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+- (void) didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
